0f06fd148b0e21f4d2a9d8187a1d03c972e11223
matshp0 <matvii.shpyruk@gmail.com>
2025-05-28 19:19:48 +0300

    minor fixes

--- e2e_tests/integration.spec.js
+++ e2e_tests/integration.spec.js
@@ -6,10 +6,13 @@ import fs from 'node:fs';
 let inputDir, outputDir;
 
 const files = [
-  { name: 'test.txt', size: 666666 },
-  { name: 'picture.png', size: 999100 },
+  { name: 'test.txt', size: 70909 },
+  { name: 'picture.png', size: 66666 },
   { name: 'program.js', size: 1919191 },
-];
+  { name: 'test1.txt', size: 70909 },
+  { name: 'pict1ure.png', size: 66666 },
+  { name: 'prog3ram.js', size: 1919191 },
+].sort((a, b) => a.name.localeCompare(b.name));
 
 test.beforeAll(async ({ }, testInfo) => {
   const browser = testInfo.project.name;
@@ -34,7 +37,8 @@ test('upload and download files', async ({ page, context }) => {
   const receivePage = await context.newPage();
   await receivePage.goto(url);
   await receivePage.getByRole('button', { name: 'Download files' }).click();
-  const receivedFiles = await awaitAllDownloads(receivePage, outputDir);
+  const receivedFiles = (await awaitAllDownloads(receivePage, outputDir))
+    .sort((a, b) => a.name.localeCompare(b.name));
   expect(receivedFiles).toEqual(files);
 });
 
